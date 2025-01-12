b7c5b383d401dfa4a27d8530f1efb289588af01f
GhidBase <grvdylans@gmail.com>
2025-01-12 12:48:36 -0700

    Add most projectList functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 class ProjectList {
     constructor() {
         this.projects = [];
+        this.localProjectList;
+    }
+
+    getLocalStorage() {
+        this.localProjectList = localStorage.getItem("projects");
+        console.log("localProjectList: " + this.localProjectList);
+        return this.localProjectList;
+    }
+
+    saveToLocalStorage() {
+        console.log("saving to local storage");
+        localStorage.setItem("projects", JSON.stringify(this.projects))
+    }
+
+    initializeProjectList() {
+        if (this.getLocalStorage()) {
+            this.projects = JSON.parse(this.localProjectList);
+        }
+        else {
+            this.saveToLocalStorage();
+        }
     }
     
     addProject(title, description, priority) {
         const newProject = new Project(title, description, priority);
         this.projects.push(newProject);
+        this.saveToLocalStorage();
     }
 
     removeProject(projectToRemove) {
@@ -29,3 +51,8 @@ class ToDo {
         this.priority = priority
     }
 }
+
+
+const projectList = new ProjectList;
+projectList.initializeProjectList();
+console.log(projectList.projects);
