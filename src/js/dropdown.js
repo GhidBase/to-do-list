29971e6c47716b272d5a589d3eb15703ad45ff39
Dylan <grvdylans@gmail.com>
2025-01-20 12:49:55 -0700

    Add a dropdown module

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -13,14 +13,17 @@ export class Dropdown {
 
   dropdownItemContainerSetup() {
     this.dropdownItemContainer.classList.add("drop-down-item-container");
-    this.appendDropdownItem("Tic-Tac-Toe");
-    this.appendDropdownItem("Lucky Defense Guides");
+    this.appendDropdownItem("Tic-Tac-Toe", "https://ghidbase.github.io/tic-tac-toe/");
+    this.appendDropdownItem("Lucky Defense Guides", "https://luckydefenseguides.com/");
   }
 
-  appendDropdownItem(title) {
+  appendDropdownItem(title, link) {
     const newItem =  document.createElement("button");
     newItem.textContent = title;
     newItem.classList.add("drop-down-item");
+    newItem.onclick = () => {
+      window.open(link, '_blank');
+    }
     this.dropdownItemContainer.appendChild(newItem);
     return newItem;
   }
